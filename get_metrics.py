a82a00317bdb2fadf6f3613fd1cdd5f8b9227071
Evan Derickson <evan.derickson@kaart.com>
2022-04-01 14:56:43 -0700

    qc or edit stats, overwrite confirm, fewer request

--- get_metrics.py
+++ get_metrics.py
@@ -2,7 +2,6 @@
 
 import argparse
 import os
-import time
 from datetime import date, timedelta
 from pathlib import Path
 from typing import Generator
@@ -10,9 +9,12 @@ from typing import Generator
 import pandas as pd
 import requests
 import yaml
+from more_itertools import chunked
 
-BASE_URL = "https://***REMOVED***/api/v2/data/user/{user}/leaderboard"
+BASE_URL = "https://***REMOVED***/api/v2/data/{mtype}/leaderboard"
 APIKEY = os.environ["APIKEY"]
+
+metric_type = {"editor": "user", "qc": "reviewer"}
 
 
 def argparsing() -> argparse.Namespace:
@@ -34,6 +36,13 @@ def argparsing() -> argparse.Namespace:
         default=date.today(),
         help="The end of the date range you wish to check.",
     )
+    parser.add_argument(
+        "--metric-type",
+        type=str.lower,
+        choices=["editor", "qc"],
+        default="editor",
+        help="Which type of metrics to get: editor or QC. Defaults to editor.",
+    )
 
     return parser.parse_args()
 
@@ -42,6 +51,7 @@ def daterange(start_date: date, end_date: date) -> Generator[date, None, None]:
     for n in range((end_date - start_date).days):
         yield start_date + timedelta(n)
 
+
 if __name__ == "__main__":
     opts = argparsing()
 
@@ -49,24 +59,39 @@ if __name__ == "__main__":
         ids = yaml.safe_load(f.read())
 
     series_of_series = []
-    for username, user in ids.items():
-        daily_tasks = pd.Series()
-        daily_tasks.name = username
-        for day in daterange(opts.start, opts.end + timedelta(1)):
+    for day in daterange(opts.start, opts.end + timedelta(1)):
+        completed_tasks = {}
+        for page in chunked(ids.values(), 50):
             r = requests.get(
-                BASE_URL.format(user=user),
+                BASE_URL.format(mtype=metric_type[opts.metric_type]),
                 headers={"apikey": APIKEY},
-                params={"start": day.isoformat(), "end": (day).isoformat()},
+                params={
+                    "start": day.isoformat(),
+                    "end": (day).isoformat(),
+                    "limit": 50,
+                    "userIds": ",".join(page),
+                },
                 verify=False,
             )
-            try:
-                completed_tasks = r.json()[0]["completedTasks"]
-            except IndexError:
-                completed_tasks = 0
-            daily_tasks[day] = completed_tasks
-            time.sleep(0.5)
-        series_of_series.append(daily_tasks)
+            page_tasks = {
+                record["name"]: record["completedTasks"] for record in r.json()
+            }
+            completed_tasks |= page_tasks
+        the_series = pd.Series(completed_tasks)
+        the_series.name = day
+        series_of_series.append(the_series)
 
     df = pd.DataFrame(series_of_series)
+    df = df.transpose()
+    df.fillna(0, inplace=True)
 
-    df.to_excel(opts.output)
+    try:
+        with opts.output.open("x") as f:
+            df.to_excel(f)
+    except FileExistsError:
+        response = input(f"{opts.output} exists. Do you want to overwrite? N/y: ")
+        if response.lower().startswith("y"):
+            with opts.output.open("w") as f:
+                df.to_excel(f)
+        else:
+            print("Save aborted.")
