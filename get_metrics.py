875431c7e99d7a685ec10d11cb4770bc7c88bed5
Evan Derickson <evan.derickson@kaart.com>
2022-04-01 11:02:06 -0700

    Parameterized stuff

--- get_metrics.py
+++ get_metrics.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import time
 from datetime import date, timedelta
+from pathlib import Path
 from typing import Generator
 
 import pandas as pd
@@ -16,6 +17,12 @@ APIKEY = os.environ["APIKEY"]
 
 def argparsing() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "ids",
+        type=Path,
+        help="The path to a yaml file containing usernames and their MR ids",
+    )
+    parser.add_argument("output", type=Path, help="Where to save the output file.")
     parser.add_argument(
         "start",
         type=date.fromisoformat,
@@ -35,33 +42,31 @@ def daterange(start_date: date, end_date: date) -> Generator[date, None, None]:
     for n in range((end_date - start_date).days):
         yield start_date + timedelta(n)
 
+if __name__ == "__main__":
+    opts = argparsing()
 
-opts = argparsing()
+    with opts.ids.open() as f:
+        ids = yaml.safe_load(f.read())
 
-with open("ids.yaml") as f:
-    ids = yaml.safe_load(f.read())
+    series_of_series = []
+    for username, user in ids.items():
+        daily_tasks = pd.Series()
+        daily_tasks.name = username
+        for day in daterange(opts.start, opts.end + timedelta(1)):
+            r = requests.get(
+                BASE_URL.format(user=user),
+                headers={"apikey": APIKEY},
+                params={"start": day.isoformat(), "end": (day).isoformat()},
+                verify=False,
+            )
+            try:
+                completed_tasks = r.json()[0]["completedTasks"]
+            except IndexError:
+                completed_tasks = 0
+            daily_tasks[day] = completed_tasks
+            time.sleep(0.5)
+        series_of_series.append(daily_tasks)
 
-series_of_series = []
-for username, user in ids.items():
-    daily_tasks = pd.Series()
-    daily_tasks.name = username
-    for day in daterange(opts.start, opts.end):
-        r = requests.get(
-            BASE_URL.format(user=user),
-            headers={"apikey": APIKEY},
-            params={"start": day.isoformat(), "end": (day + timedelta(1)).isoformat()},
-            verify=False,
-        )
-        try:
-            completed_tasks = r.json()[0]["completedTasks"]
-        except IndexError:
-            completed_tasks = 0
-        daily_tasks[day] = completed_tasks
-        time.sleep(1)
-    series_of_series.append(daily_tasks)
+    df = pd.DataFrame(series_of_series)
 
-df = pd.DataFrame(series_of_series)
-
-df.to_excel(
-    "metrics.xlsx"
-)
+    df.to_excel(opts.output)
